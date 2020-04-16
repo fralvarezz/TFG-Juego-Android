5eecbd951b2c86f5c76108148c3864ebbcf4772f
fralvarezz <56074355+fralvarezz@users.noreply.github.com>
2020-04-16 11:52:11 +0200

    Removed unnecessary commented code

--- Assets/Scripts/DestructibleCollect.cs
+++ Assets/Scripts/DestructibleCollect.cs
@@ -13,39 +13,8 @@ public class DestructibleCollect : MonoBehaviour
     private void Start()
     {
         startPosition = (Vector2) transform.position;
-        /*foreach (Transform child in transform)
-        {
-            if (child.name == "Platform")
-            {
-                platform = child.GetComponent<BoxCollider2D>();
-            }
-            else if (child.name == "FaceHitbox")
-            {
-                face = child.GetComponent<BoxCollider2D>();
-            }
-        }*/
     }
-
-    /*
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        Character player = other.gameObject.GetComponent<Character>();
-        
-        if (player != null)
-        {
-            if (player.IsDashing)
-            {
-                transform.position = startPosition;
-                player.IsDashing = false;
-                player.ReloadDashes();
-            }
-            else
-            {
-                player.PlayerDied();
-            }
-        }
-    }
-    */
+    
     
     private void OnTriggerEnter2D(Collider2D other)
     {
