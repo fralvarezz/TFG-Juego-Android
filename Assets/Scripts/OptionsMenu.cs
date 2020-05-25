1e65fa60a8ef8112827386cecad53dbd64a6a551
fralvarezz <56074355+fralvarezz@users.noreply.github.com>
2020-05-25 16:13:48 +0200

    Added sound effects and music toggles

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -9,20 +9,78 @@ public class OptionsMenu : MonoBehaviour
 {
     public Slider volSlider;
 
+    public Toggle soundEffectsToggle;
+    public Toggle musicToggle;
+
     public AudioMixer audioMixer;
+
     
     private void Start()
     {
         volSlider.value = PlayerPrefs.GetFloat("MasterVolume", 0f);
         audioMixer.SetFloat("volume", volSlider.value);
         PlayerPrefs.SetFloat("MasterVolume", volSlider.value);
-        PlayerPrefs.Save();
+        
+        soundEffectsToggle.isOn = Convert.ToBoolean(PlayerPrefs.GetInt("SoundEffectsToggle", 1));
+        if (soundEffectsToggle.isOn)
+        {
+            audioMixer.SetFloat("soundEffectsVolume", 0f);
+            PlayerPrefs.SetInt("SoundEffectsToggle", 1);
+        }
+        else
+        {
+            audioMixer.SetFloat("soundEffectsVolume", -80f);
+            PlayerPrefs.SetInt("SoundEffectsToggle", 0);
+        }
+        
+        
+        musicToggle.isOn = Convert.ToBoolean(PlayerPrefs.GetInt("MusicToggle", 1));
+        if (musicToggle.isOn)
+        {
+            audioMixer.SetFloat("musicVolume", 0f);
+            PlayerPrefs.SetInt("MusicToggle", 1);
+        }
+        else
+        {
+            audioMixer.SetFloat("musicVolume", -80f);
+            PlayerPrefs.SetInt("MusicToggle", 0);
+        }
+        
     }
 
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
         PlayerPrefs.SetFloat("MasterVolume", volume);
-        //PlayerPrefs.Save();
     }
+
+    public void MuteUnmuteSoundEffects()
+    {
+        if (!soundEffectsToggle.isOn)
+        {
+            audioMixer.SetFloat("soundEffectsVolume", -80f);
+            PlayerPrefs.SetInt("SoundEffectsToggle", 0);
+        }
+        else
+        {
+            audioMixer.SetFloat("soundEffectsVolume", 0f);
+            PlayerPrefs.SetInt("SoundEffectsToggle", 1);
+        }
+    }
+
+    public void MuteUnmuteMusic()
+    {
+        if (!musicToggle.isOn)
+        {
+            audioMixer.SetFloat("musicVolume", -80f);
+            PlayerPrefs.SetInt("MusicToggle", 0);
+        }
+        else
+        {
+            audioMixer.SetFloat("musicVolume", 0f);
+            PlayerPrefs.SetInt("MusicToggle", 1);
+        }
+        
+    }
+
 }
