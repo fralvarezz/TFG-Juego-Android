9a93cd9ed0fb3c692e0eb6d6d41a6fd728aa2c44
fralvarezz <56074355+fralvarezz@users.noreply.github.com>
2020-05-25 12:27:16 +0200

    Implemented save player settings

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,14 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
+    public Slider volSlider;
+
     public AudioMixer audioMixer;
     
+    private void Start()
+    {
+        volSlider.value = PlayerPrefs.GetFloat("MasterVolume", 0f);
+        audioMixer.SetFloat("volume", volSlider.value);
+        PlayerPrefs.SetFloat("MasterVolume", volSlider.value);
+        PlayerPrefs.Save();
+    }
+
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
+        PlayerPrefs.SetFloat("MasterVolume", volume);
+        //PlayerPrefs.Save();
     }
 }
