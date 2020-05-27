280080d45cd1e7e14cb9eb07f916509cc8ceacf1
fralvarezz <56074355+fralvarezz@users.noreply.github.com>
2020-05-27 12:50:01 +0200

    Added GPS login

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,9 +3,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using GooglePlayGames.BasicApi;
+using GooglePlayGames;
+using TMPro;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    private TMP_Text signInButtonText;
+    public Button signInButton;
+    public Text authStatus;
+    
+    void Start()
+    {
+
+        signInButtonText = signInButton.GetComponentInChildren<TMP_Text>();
+        
+        Debug.Log(signInButtonText.text);
+        
+        // Create client configuration
+        PlayGamesClientConfiguration config = new 
+                PlayGamesClientConfiguration.Builder()
+            .Build();
+
+        // Enable debugging output (recommended)
+        PlayGamesPlatform.DebugLogEnabled = true;
+        
+        // Initialize and activate the platform
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+        
+        // Try silent sign-in (second parameter is isSilent)
+        PlayGamesPlatform.Instance.Authenticate(SignInCallback, true);
+    }
+    
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -23,5 +54,40 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void SignIn()
+    {
+        if (!PlayGamesPlatform.Instance.localUser.authenticated) {
+            // Sign in with Play Game Services, showing the consent dialog
+            // by setting the second parameter to isSilent=false.
+            PlayGamesPlatform.Instance.Authenticate(SignInCallback, false);
+        } else {
+            // Sign out of play games
+            PlayGamesPlatform.Instance.SignOut();
+            
+            // Reset UI
+            signInButtonText.text = "LOGIN";
+            authStatus.text = "";
+        }
+    }
+    
+    private void SignInCallback(bool success) {
+        if (success) {
+            Debug.Log("(Rockpunch) Signed in!");
+
+            // Change sign-in button text
+            signInButtonText.text = "LOGOUT";
+            
+            // Show the user's name
+            authStatus.text = "Logeado como: " + Social.localUser.userName;
+        } else {
+            Debug.Log("(Rockpunch) Sign-in failed...");
+            
+            // Show failure message
+            signInButtonText.text = "LOGIN";
+            authStatus.text = "Login fallido";
+        }
+    }
+    
     
 }
